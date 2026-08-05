080cb25f50d820424fb71aaf970509534f73e6ed
Saketh Subramanian <subramanian.saketh@gmail.com>
2026-08-05 11:47:44 -0400

    Day 32: UVM Driver and Monitor: Sequencer handshake and monitor analysis port

--- tb/uvm/alu_driver.sv
+++ tb/uvm/alu_driver.sv
@@ -1,3 +1,29 @@
-class alu_driver extends uvm_driver;
-    `uvm_component_utils(alu_driver)
+class alu_driver extends uvm_driver #(cmd_transaction);
+    `uvm_component_utils(alu_driver);
+
+    virtual dut_if.TB vif;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        // comment placeholder for uvm_config_db::get() to retrieve virtual interface
+    endfunction
+
+    virtual task run_phase (uvm_phase phase);
+        cmd_transaction cmd_trans;
+
+        forever begin
+            wait (!vif.full)
+            seq_item_port.get_next_item(cmd_trans);
+            vif.tb_cb.write_data <= {cmd_trans.opcode, cmd_trans.a, cmd_trans.b};
+            vif.tb_cb.write_enable <= 1;
+            @(vif.tb_cb)
+            vif.tb_cb.write_enable <= 0;
+            
+            seq_item_port.item_done();
+        end
+    endtask
 endclass
--- tb/uvm/alu_monitor.sv
+++ tb/uvm/alu_monitor.sv
@@ -1,3 +1,33 @@
 class alu_monitor extends uvm_monitor;
-    `uvm_component_utils(alu_monitor)
+    `uvm_component_utils(alu_monitor);
+    
+    virtual dut_if.TB vif;
+
+    uvm_analysis_port #(cmd_transaction) mon_analysis_port;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase (uvm_phase phase);
+        super.build_phase(phase);
+        mon_analysis_port = new("mon_analysis_port", this);
+        // comment placeholder for uvm_config_db::get() to retrieve virtual interface
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        cmd_transaction cmd_trans;
+
+        forever begin
+            @(posedge vif.result_valid)
+            cmd_trans = cmd_transaction::type_id::create("cmd_trans", this);
+            cmd_trans.exp_result = vif.result;
+            cmd_trans.exp_zero = vif.zero;
+            cmd_trans.exp_negative = vif.negative;
+            cmd_trans.exp_carry_out = vif.carry_out;
+            cmd_trans.exp_cmp_out = vif.cmp_out;
+            mon_analysis_port.write(cmd_trans);
+            @(negedge vif.result_valid)
+        end
+    endtask
 endclass
