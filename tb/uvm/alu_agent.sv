3c92c8b279a0085ed15526f28e2da76c1d17497e
Saketh Subramanian <subramanian.saketh@gmail.com>
2026-08-08 01:13:46 -0400

    Day 34: uvm_agent + uvm_env --> Component creations and TLM port connections

--- tb/uvm/alu_agent.sv
+++ tb/uvm/alu_agent.sv
@@ -1,3 +1,32 @@
 class alu_agent extends uvm_agent;
-    `uvm_component_utils(alu_agent)
+    `uvm_component_utils(alu_agent);
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    alu_driver driver;
+    alu_monitor monitor;
+    alu_sequencer sequencer;
+
+    virtual function void build_phase (uvm_phase phase);
+        super.build_phase(phase);
+
+        if (get_is_active()) begin
+            driver = alu_driver::type_id::create("driver", this);
+            sequencer = alu_sequencer::type_id::create("sequencer", this);
+        end
+
+        monitor = alu_monitor::type_id::create("monitor", this);
+
+    endfunction
+
+    virtual function void connect_phase (uvm_phase phase);
+        super.connect_phase(phase);
+
+        if (get_is_active()) begin
+            driver.seq_item_port.connect(sequencer.seq_item_export);
+        end
+
+    endfunction
 endclass
--- tb/uvm/alu_env.sv
+++ tb/uvm/alu_env.sv
@@ -1,3 +1,22 @@
 class alu_env extends uvm_env;
-    `uvm_component_utils(alu_env)
+    `uvm_component_utils(alu_env);
+
+    alu_agent agent1;
+    scoreboard scoreboard1;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        agent1 = alu_agent::type_id::create("agent", this);
+        scoreboard1 = scoreboard::type_id::create("scoreboard", this);
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+
+        agent1.monitor.mon_analysis_port.connect(scoreboard1.ap_imp); // connects to uvm_analysis_imp in scoreboard
+    endfunction
 endclass
